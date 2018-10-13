0bc6b9190e70b2c74b7d3020b3d1ea910b21044c
Clément <baudet.clement@me.com>
2018-10-13 08:01:15 +0200

    before rewriting

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,7 +5,44 @@ var idInterval = null;
 var data = null;
 let images = [];
 let seconds = 0;
+let emotions = [];
+let level;
+
+
+const repartitions  =  [
+  {"D1": 0.6, "D2": 0.3, "D3": 0.1},
+  {"D1": 0.33, "D2": 0.34, "D3": 0.33},
+  {"D1": 0.1, "D2": 0.3, "D3": 0.6},
+]
+
 window.SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition;
+
+
+function getScoreFromEmotion(emotions) {
+  let maxValue = 0;
+  let maxLabel;
+  for(let emotion of emotions) {
+    if(emotion.Confidence>maxValue) {
+      maxValue = emotion.Confidence
+      maxLabel = emotion.Type
+    }
+  }
+  switch(maxLabel) {
+    case "HAPPY":
+    case "CALM": 
+      return 10;
+    case "SURPRISED":
+      return 8;
+    case "DISGUSTED":
+      return 6;
+    case "ANGRY":
+      return 2;
+    case "SAD": 
+      return 1;
+    default:
+      return 0;
+  }
+}
 
 
 function updateDOM(obj){
@@ -56,6 +93,9 @@ window.onload = function(){
       const speechToText = event.results[i][0].transcript;
       i++;
       console.log("Texte enfant: " + speechToText)
+      if(speechToText === "verre") {
+        cb("vert")
+      }
       cb(speechToText)
     }
   }
@@ -79,15 +119,15 @@ window.onload = function(){
       talk(question.name)
       displayQuestion(question).then((time) => {
         console.log("YEAHH en " + time)
-        resolve()
+        resolve(10000)
       }).catch((time) => {
         talk(question.name)
         displayQuestion(question).then(() => {
-        resolve()
+        resolve(10000)
         }).catch(() => {
           talk(question.name)
           talk("Tant pis, tu réessaieras plus tard.")
-          resolve()
+          resolve(10000)
         })
       })
     });
@@ -101,28 +141,40 @@ window.onload = function(){
     rand = Math.floor(Math.random()*data.D3.length)
     images.push(data.D3[rand])
     recognition.start()
-
+    let moyenne = 0;
     displayQuestion(images[0]).then((time) => {
       console.log("YEAHH IMG1 en " + time)
+      moyenne = (moyenne + (10000 - (time))/1000)/2;
       displayQuestion(images[1]).then((time) => {
         console.log("YEAHH IMG2 en " + time)
+        moyenne = (moyenne + (10000 - (time))/1000)/2;
         displayQuestion(images[2]).then((time) => {
           console.log("YEAHH IMG3 en " + time)
+          moyenne = (moyenne + (10000 - (time))/1000)/2;
+          calculScore(moyenne)
         }).catch((time) => {
-          console.log("Faux IMG3 en " + time)
-          repeatQuestion(images[2]).then(
-            console.log("fini")
-          )
+          if(time !== 0){ 
+            repeatQuestion(images[2]).then((time) => {
+            moyenne = (moyenne + (10000 - (time))/1000)/2; 
+            calculScore(moyenne)
+            })
+          }
         })
       }).catch((time) => {
         repeatQuestion(images[1]).then((time) => {
           displayQuestion(images[2]).then((time) => {
             console.log("YEAHH IMG3 en " + time)
+            moyenne = (moyenne + (10000 - (time))/1000)/2;
+            calculScore(moyenne)
           }).catch((time) => {
-            console.log("Faux IMG3 en " + time)
-            repeatQuestion(images[2]).then(() => {
-              console.log("fini")
-            })
+            if(time !== 0){ 
+              console.log("Faux IMG3 en " + time)
+              moyenne = (moyenne + (10000 - (time))/1000)/2;
+              repeatQuestion(images[2]).then((time) => {
+                moyenne = (moyenne + (10000 - (time))/1000)/2;
+                calculScore(moyenne)
+              })
+          }
           })
         })
       })
@@ -130,23 +182,32 @@ window.onload = function(){
       repeatQuestion(images[0]).then(() => {
         displayQuestion(images[1]).then((time) => {
           console.log("YEAH IMG2 en "+ time)
+          moyenne = (moyenne + (10000 - (time))/1000)/2;
           displayQuestion(images[2]).then((time) => {
             console.log("YEAHH IMG3 en " + time)
+            moyenne = (moyenne + (10000 - (time))/1000)/2;
           }).catch((time) => {
-            console.log("Faux IMG3 en " + time)
-            repeatQuestion(images[2]).then(() => {
-              console.log("fini")
-            })
+            if(time !== 0){ 
+              console.log("Faux IMG3 en " + time)
+              repeatQuestion(images[2]).then((time) => {
+                moyenne = (moyenne + (10000 - (time))/1000)/2;
+                calculScore(moyenne)
+              })
+          }
           })
         }).catch((time) => {
           repeatQuestion(images[1]).then((time) => {
             displayQuestion(images[2]).then((time) => {
               console.log("YEAHH IMG3 en " + time)
+              moyenne = (moyenne + (10000 - (time))/1000)/2;
             }).catch((time) => {
-              console.log("Faux IMG3 en " + time)
-              repeatQuestion(images[2]).then(() => {
-                console.log("fini")
-              })
+              if(time !== 0){ 
+                console.log("Faux IMG3 en " + time)
+                repeatQuestion(images[2]).then((time) => {
+                  moyenne = (moyenne + (10000 - (time))/1000)/2;
+                  calculScore(moyenne)
+                })
+              }
             })
           })
         })
@@ -154,32 +215,59 @@ window.onload = function(){
     })
   }
   
+  function calculScore(moyenne) {
+    console.log("FINISH")
+    let scoreEmotions = (emotions.reduce((a, b) => {return a+b}))/emotions.length
+    let scoreTime = moyenne
+    let score = scoreEmotions + scoreTime
+    determineLevel(score)
+    launchExercices()
+  }
+
+  function launchExercices() {
+    console.log(level)
+  }
+
+  function determineLevel(score) {
+    if(score >= 15) {
+      level = 3;
+    } else if(score >= 10) {
+      level = 2;
+    }else {
+      level = 1;
+    }
+  }
 
   let displayQuestion = function(question) {
       return new Promise(function(resolve, reject) { 
         let description = question.name.normalize('NFD').replace(/[\u0300-\u036f]/g, "");
         updateDOM(question);
         let startDate = new Date();
-        let emotions = extractEmotions()
-        let x = setInterval(() => {
-            clearInterval(x)
-            console.log("Temps imparti")
-            reject(0)
-        }, 20000);
-        dictate(function(data) {
-          clearInterval(x)
-          let success = data.split(" ").includes(description.toLowerCase())
-          let endDate = new Date();
-          if(success) {
-            resolve(endDate-startDate)
-          } else {
-            reject(endDate-startDate)
-          }
+        extractEmotions(function(data) {
+          emotions.push(getScoreFromEmotion(data))
+        
+        })
+          console.log(data)
+          let x = setTimeout(() => {
+              console.log("Temps imparti")
+              clearTimeout(x)
+              reject(0)
+          }, 20000);
+          dictate(function(data) {
+            clearTimeout(x)
+            let success = data.split(" ").includes(description.toLowerCase())
+            let endDate = new Date();
+            if(success) {
+              resolve(endDate-startDate)
+            } else {
+              reject(endDate-startDate)
+            }
+        })
       })
-    });
-  }
+    };
+  
 
-  function extractEmotions() {
+  function extractEmotions(cb) {
     canvas.setAttribute('width', VIDEOWIDTH);
     canvas.setAttribute('height', VIDEOHEIGHT);
     canvas.getContext('2d').drawImage(video, 0, 0);
@@ -194,7 +282,7 @@ window.onload = function(){
     })
     .then((response) => response.json())
     .then((data)=>{
-      return data
+      cb(data)
     })
     .catch(error => {
       console.log("error")
